e5fb040ecc1ddbabf77fe2f381f08b65e27c78b9
user-PC\user <jsme111@naver.com>
2017-05-18 22:34:43 +0900

    - 검색 기능 추가

--- spring-blog/src/main/java/com/millky/blog/application/configuration/BlogConfig.java
+++ spring-blog/src/main/java/com/millky/blog/application/configuration/BlogConfig.java
@@ -1,12 +1,27 @@
 package com.millky.blog.application.configuration;
 
 import org.h2.server.web.WebServlet;
+import org.springframework.beans.factory.config.ServiceLocatorFactoryBean;
 import org.springframework.boot.context.embedded.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import com.millky.blog.domain.service.factory.SearchServiceFactory;
+
 @Configuration
 public class BlogConfig {
+	
+	@Bean
+	public ServiceLocatorFactoryBean searchFactoryServiceLocatorFactoryBean() {
+		ServiceLocatorFactoryBean bean = new ServiceLocatorFactoryBean();
+		bean.setServiceLocatorInterface(SearchServiceFactory.class);
+		return bean;
+	}
+	
+	@Bean
+	public SearchServiceFactory searchServiceFactory() {
+		return (SearchServiceFactory) searchFactoryServiceLocatorFactoryBean().getObject();
+	}
 	@Bean
 	public ServletRegistrationBean h2servletRegistration() {
 		ServletRegistrationBean registration = new ServletRegistrationBean(new WebServlet());
